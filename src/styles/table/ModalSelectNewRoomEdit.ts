4c75a77ed89994e89cd8ff5725ed5f9e224fddc0
carlos123asd <carlos-medin@hotmail.com>
2024-09-07 00:40:03 +0200

    TypeScript add: styled/all proyect TS

--- src/styles/table/ModalSelectNewRoomEdit.ts
+++ src/styles/table/ModalSelectNewRoomEdit.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components"
 
-export const ModalSelectNewRoomEdit = styled`
+export const ModalSelectNewRoomEdit = styled.div`
    border: 1px solid red;
    width: 50%;
    height: 80%;
