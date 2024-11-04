4c62b68bac8f266a9346019ac8bf751489c1b58a
carlos123asd <carlos-medin@hotmail.com>
2024-11-04 20:39:00 +0100

    FIX: declaration imgs

--- server/src/interfaces/Message.ts
+++
@@ -1,9 +0,0 @@
-export default interface Message{
-    date:Date
-    customer:string
-    email:string
-    phone:string
-    reason:string
-    comment:string
-    status:'none'|'published'|'archived'
-}
--- server/src/interfaces/uservalid.ts
+++
@@ -1,4 +0,0 @@
-export default interface uservalid {
-    username: string,
-    password: any
-}
