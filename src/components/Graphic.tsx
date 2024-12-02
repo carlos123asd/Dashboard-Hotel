3ae91bd01488b0d26fc3aab41439d2179e9438eb
carlos123asd <carlos-medin@hotmail.com>
2024-12-02 18:28:59 +0100

    ADD: localstorage goal

--- src/components/Graphic.tsx
+++ src/components/Graphic.tsx
@@ -1,14 +1,26 @@
 import { BarChart, Card } from "@tremor/react";
-import { appSelector } from "../features/hooks/hooks";
-import { Input, InputLabel } from "@mui/material";
-import { useState } from "react";
+import { Button, Input } from "@mui/material";
+import { useEffect, useState } from "react";
 
 export default function Graphic({data}:any){
-  const [goal,setGoal] = useState(1000);
+  const [goal,setGoal] = useState(localStorage.getItem("goalMiranda") ? Number(localStorage.getItem("goalMiranda")) : 1000);
+  const [media,setMedia] = useState(333);
+
+  useEffect(() => {
+    const calc = Math.round(goal / 3)
+    setMedia(calc)
+  },[goal])
+
+  const setValueLocalGoal = (val:string) => {
+    localStorage.setItem("goalMiranda",val)
+  }
+  
+  const calculateColor = (data:any) => {
+    return data > (media * 2) ? "green" : "yellow"
+  }
       return (
         <Card className="h-full flex gap-9 flex-col">
-          <div>
-            <InputLabel htmlFor="order">Goal</InputLabel>
+          <div className="flex gap-3">
             <Input
               placeholder="Enter Goal"
               id="order"
@@ -17,6 +29,9 @@ export default function Graphic({data}:any){
               className="mt-2"
               onChange={(e) => setGoal(Number(e.target.value))}
             />
+            <Button onClick={() => setValueLocalGoal(goal ? goal.toString() : "1000")} variant="contained" size="small">
+              Save Goal
+            </Button>
           </div>
           <BarChart
             maxValue={goal}
@@ -25,7 +40,7 @@ export default function Graphic({data}:any){
             index="date"
             categories={["gains"]}
             showLegend={false}
-            colors={["yellow"]} //Hacer calculo para cambiar colores segun el progreso
+            colors={[data[0].gains < media ? "red" : calculateColor(data[0].gains)]}
           />
         </Card>
       )
